adf19aedf9489aedb177487c1c78b8acdc3262c8
khalbig <khalbig@gmail.com>
2024-06-02 14:02:43 -0700

    Implemented Fuzzy Wuzzy and fixed reading it rav_rec to make patterns the row index.

--- code/streamlit_code.py
+++ code/streamlit_code.py
@@ -1,15 +1,22 @@
 import pandas as pd
 import streamlit as st
-import pickle
+from fuzzywuzzy import fuzz 
+from fuzzywuzzy import process 
 
-rav_rec_df = pd.read_pickle('../data/rav_rec.pkl')
+rav_rec_df = pd.read_csv('../data/rav_rec.csv', index_col = 'pattern_name')
 
 txt = st.text_area("Enter a knitting pattern you like!  Make sure it's written exactly how the designer formatted the name.").strip()
 
 
 if st.button('Submit'):
+    choices = rav_rec_df.columns
+    fuzzy_process = process.extractOne(txt, choices)
+    fuzzy_choice = fuzzy_process[0]
+    st.write(f'We think you are looking for the pattern titled: {fuzzy_choice}')
+    
     try:
-        top_five = list(rav_rec_df[txt].sort_values().iloc[1:6].index)
+        top_five = list(rav_rec_df[fuzzy_choice].sort_values().iloc[1:6].index)
+        print(f'This is Top Five: {top_five}')
 
         # Make a dictionary to deal with characters in the pattern name, but not in the url address.
         replacements = {
@@ -26,15 +33,11 @@ if st.button('Submit'):
 
         # Make a table of the replacements dictionary using .maketrans
         replacement_table = str.maketrans(replacements)
-
+    
         #Iterate through top five patterns to transform names into their url equivalents
-        urls = []
         for patt in top_five:
             url_ready = patt.translate(replacement_table).lower()
             url = f'{patt}: https://www.ravelry.com/patterns/library/{url_ready}'
-            urls.append(url)
-
-        for url in urls:
             st.write(url)
 
     # Return error message if typo or non-existent pattern
